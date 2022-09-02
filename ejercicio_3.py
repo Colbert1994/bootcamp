8c1fc2430a99bec6a68f1924e168ea908527f49a
Jhin <im.jhin@gmail.com>
2022-09-02 18:08:02 -0300

    ejercicio 3

---
+++ ejercicio_3.py
@@ -0,0 +1,6 @@
+# En este ejercicio tendrás que crear una variable a la cual asignarás una cadena de texto y la imprimirás por consola, 
+# a su vez, tendrás que imprimir también el tipo de la variable por consola.
+
+# Creacion de variable con cadena de texto.
+cad_texto_1 = 'cadena de texto'
+print(cad_texto_1, type(cad_texto_1))
