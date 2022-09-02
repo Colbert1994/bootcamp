287974078cd6522bc197e3da26a71c97dfed8646
Jhin <im.jhin@gmail.com>
2022-09-02 17:26:06 -0300

    ejercicio print

---
+++ ejercicios.py
@@ -0,0 +1,13 @@
+# Desde la consola de python, debes de imprimir las siguientes cadenas de texto:
+
+# Hola, soy [tu_nombre]
+
+# Estoy empezando el curso de python
+
+# Espero aprender mucho
+
+# Tienes que subir capturas de pantalla en una carpeta comprimida zip
+
+print('Hola, soy Colbert')
+print('Estoy empezando el curso de python')
+print('Espero aprender mucho')
